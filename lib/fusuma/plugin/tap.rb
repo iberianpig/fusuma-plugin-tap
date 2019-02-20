7d97c310613e3dd866a70a167c8fe645e10795b6
iberianpig <yhkyky@gmail.com>
2019-02-21 02:03:54 +0900

    add parser

--- lib/fusuma/plugin/tap.rb
+++ lib/fusuma/plugin/tap.rb
@@ -1,11 +1,3 @@
 require "fusuma/plugin/tap/version"
 require "fusuma/plugin/vectors/tap_vector"
-
-module Fusuma
-  module Plugin
-    module Tap
-      class Error < StandardError; end
-      # Your code goes here...
-    end
-  end
-end
+require "fusuma/plugin/parsers/tap_parser"
--- lib/fusuma/plugin/vectors/tap_vector.rb
+++ lib/fusuma/plugin/vectors/tap_vector.rb
@@ -1,57 +1,57 @@
-require 'fusuma/vector'
-
 module Fusuma
-  module Vectors
-    # vector data
-    class TapVector < BaseVector
-      TYPE = 'tap'.freeze
-      GESTURE = 'tap'.freeze
+  module Plugin
+    module Vectors
+      # vector data
+      class TapVector < Vector
+        TYPE = 'tap'.freeze
+        GESTURE = 'tap'.freeze
 
-      BASE_INTERVAL   = 0.1
+        BASE_INTERVAL = 0.1
 
-      def initialize(finger, status)
-        @finger = finger.to_i
-        @status = status
-      end
-      attr_reader :finger, :status
+        def initialize(finger, status)
+          @finger = finger.to_i
+          @status = status
+        end
+        attr_reader :finger, :status
 
-      def enough?
-        MultiLogger.debug(self)
-        released? && enough_interval?
-      end
+        def enough?
+          MultiLogger.debug(self)
+          released? && enough_interval?
+        end
 
-      private
+        private
 
-      def released?
-        status == 'pressed'
-      end
+        def released?
+          status == 'pressed'
+        end
 
-      def enough_interval?
-        return true if first_time?
-        return true if (Time.now - self.class.last_time) > interval_time
+        def enough_interval?
+          return true if first_time?
+          return true if (Time.now - self.class.last_time) > interval_time
 
-        false
-      end
+          false
+        end
 
-      def first_time?
-        !self.class.last_time
-      end
+        def first_time?
+          !self.class.last_time
+        end
 
-      def interval_time
-        @interval_time ||= BASE_INTERVAL * Config.interval(self)
-      end
+        def interval_time
+          @interval_time ||= BASE_INTERVAL * Config.interval(self)
+        end
 
-      class << self
-        attr_reader :last_time
+        class << self
+          attr_reader :last_time
 
-        def generate(event_buffer:)
-          return if event_buffer.gesture != GESTURE
-          return if Generator.prev_vector && Generator.prev_vector != self
+          def generate(event_buffer:)
+            return if event_buffer.gesture != GESTURE
+            return if Generator.prev_vector && Generator.prev_vector != self
 
-          new(event_buffer.finger, event_buffer.events.last.record.status).tap do |v|
-            return nil unless v.enough?
+            new(event_buffer.finger, event_buffer.events.last.record.status).tap do |v|
+              return nil unless v.enough?
 
-            Generator.prev_vector = self
+              Generator.prev_vector = self
+            end
           end
         end
       end
